5fc9757bb63a71800f61a0b2b04a90d01a076c41
proomguy <mattz100bc@gmail.com>
2023-02-15 04:03:24 +0300

    My first Commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/pages/login.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(future: _initialization,
+    builder: (context, snapshot){
+        if(snapshot.hasError){
+          print("Something went wront");
+        }
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Center(child: CircularProgressIndicator());
+        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Email & Password',
+          theme: ThemeData(
+            primarySwatch: Colors.cyan,
+          ),
+          home: LoginPage(),
+        );
+      },
+    );
+  }
+}
+
---
+++ lib/pages/login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>{
+
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
