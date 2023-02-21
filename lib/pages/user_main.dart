2dcc0a120255b2909fa29f586d2745519dd54213
proomguy <mattz100bc@gmail.com>
2023-02-21 16:09:48 +0300

    Implementing bottom navigation bar

--- lib/pages/user_main.dart
+++ lib/pages/user_main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/user/changed_password.dart';
+import 'package:flutter_firebase_app/user/dashboard.dart';
+import 'package:flutter_firebase_app/user/profile.dart';
+//import 'package:flutter/cupertino.dart';
 
 class UserMain extends StatefulWidget {
   const UserMain({Key? key}) : super(key: key);
@@ -8,8 +12,43 @@ class UserMain extends StatefulWidget {
 }
 
 class _UserMainState extends State<UserMain> {
+
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[
+    Dashboard(),
+    ChangePassword(),
+    Profile(),
+  ];
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+            label: ' Dashboard '
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: ' Profile '
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: ' Changed Password '
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
